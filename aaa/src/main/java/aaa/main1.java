bc6c28ececfc8dabfdc86a0b6ea91a9f60893189
Keigo <Keigo@LAPTOP-9NK78MMD>
2021-07-20 00:43:33 +0900

    足し算追加

--- aaa/src/main/java/aaa/main1.java
+++ aaa/src/main/java/aaa/main1.java
@@ -3,6 +3,11 @@ package aaa;
 public class main1 {
 	public static void main(String[] args) {
 		System.out.println("hello-keigoさん");
+		int a;
+		int b;
+		a = 2000;
+		b = a + 4000;
+		System.out.println(b);
 	}
 
 }
